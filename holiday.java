005632c5c284e0b491ef1e995aac99c1a7832a62
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-16 17:26:01 +0530

    Add files via upload

---
+++ holiday.java
@@ -0,0 +1,27 @@
+package set2;
+
+public class holiday {
+public static void main(String[] args) {
+	if(args[0].equalsIgnoreCase("sunday")){
+		System.out.println("yes");
+	}
+	if(args[0].equalsIgnoreCase("monday")){
+		System.out.println("no");
+	}
+	if(args[0].equalsIgnoreCase("tuesday")){
+		System.out.println("no");
+	}
+	if(args[0].equalsIgnoreCase("wednesday")){
+		System.out.println("no");
+	}
+	if(args[0].equalsIgnoreCase("thursday")){
+		System.out.println("no");
+	}
+	if(args[0].equalsIgnoreCase("friday")){
+		System.out.println("no");
+	}
+	if(args[0].equalsIgnoreCase("saturday")){
+		System.out.println("yes");
+	}
+}
+}
