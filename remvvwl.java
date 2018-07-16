d2290495360a7f55a6804f1f438a02e4ba3d6838
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-16 17:43:09 +0530

    Add files via upload

---
+++ remvvwl.java
@@ -0,0 +1,8 @@
+package player;
+
+public class remvvwl {
+public static void main(String[] args) {
+	String rev=args[0].replaceAll("[aeiouAEIOU]","");
+	System.out.println(new StringBuffer(rev).reverse().toString());	
+}
+}
