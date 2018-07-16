8a60a7e612333e1aac140b36cf3e01d6d44d8224
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-16 15:37:43 +0530

    Add files via upload

---
+++ reploded.java
@@ -0,0 +1,20 @@
+package set2;
+
+public class reploded {
+public static void main(String[] args) {
+	char c[]=args[0].toCharArray();
+	for(int i=0;i<args[0].length();i=i+2){
+		if(i%2==0){
+			char c1=c[i];
+			c[i]=c[i+1];
+			c[i+1]=c1;
+		}
+		else {
+			char c1=c[i];
+			c[i]=c[i+1];
+			c[i+1]=c1;
+		}
+		System.out.print(c[i]+""+c[i+1]);
+	}
+}
+}
